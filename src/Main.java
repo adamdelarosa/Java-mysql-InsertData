b4c7b3ae0afb2756834fdc256ff904d4958281bf
ROSA <gojiradam@gmail.com>
2016-03-28 20:39:06 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ROSA on 3/28/16.
+ */
+public class Main {
+    public static void main(String args[]){
+        printer();
+    }
+
+    public static void printer(){
+        System.out.println("Hello");
+    }
+}
